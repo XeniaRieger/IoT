1b893e0900f2520b09f465dbfba56d38e370a30f
mena-py <menarbin00@gmail.com>
2021-12-09 11:33:16 +0100

    Try

--- BeaconDetection/app/src/main/java/com/example/beacondetection/MainActivity.java
+++ BeaconDetection/app/src/main/java/com/example/beacondetection/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     private static final int PERMISSION_REQUEST_FINE_LOCATION = 1;
     private static final int PERMISSION_REQUEST_BACKGROUND_LOCATION = 2;
 
-    public String a; //TO DELETEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+    public String aa; //TO DELETEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
 
     private BeaconManager beaconManager = null;
     protected static final String TAG = "MonitoringActivity";
